009cb179ac1457824907a9cb4ab1f0772a61c04c
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2021-12-15 17:42:55 +0100

    manfred_flühmann

--- scripts/scaleImages/find.sh
+++ scripts/scaleImages/find.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /Users/toesch/Sites/k/pn44/frontend/public/uploads -type f -iname "*01c376845262ebdd8fc3919ced3f4a080925e41839fcc62f4dbf2550408519d0*" -exec mogrify -verbose -path /Users/toesch/Sites/k/pn44/frontend/public/kandis -format jpeg -layers Dispose -resize 400\>x1024\> {} +
+find /Users/toesch/Sites/k/pn44/frontend/public/uploads -type f -iname "*a75ee9c3c718e0c2d3b13c0084e48f2a4b3820af44cd1622b0ec6a04e2c12df3*" -exec mogrify -verbose -path /Users/toesch/Sites/k/pn44/frontend/public/kandis -format jpeg -layers Dispose -resize 400\>x1024\> {} +
