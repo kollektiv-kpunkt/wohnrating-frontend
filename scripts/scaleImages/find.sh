f282602543379d3f726b620f27c27cf9f441b96e
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2021-12-16 09:54:54 +0100

    kandis202112160954

--- scripts/scaleImages/find.sh
+++ scripts/scaleImages/find.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /Users/toesch/Sites/k/pn44/frontend/public/uploads -type f -iname "*3ee3b2e9cced99f0886a3fb5a181b1866c7746bd2f733492cf8d46580d556a3e*" -exec mogrify -verbose -path /Users/toesch/Sites/k/pn44/frontend/public/kandis -format jpeg -layers Dispose -resize 400\>x1024\> {} +
+find /Users/toesch/Sites/k/pn44/frontend/public/uploads -type f -iname "*fdf026ff5d52b60631f4a2fc7c065a318af944667b986a4d888713b69765eba3*" -exec mogrify -verbose -path /Users/toesch/Sites/k/pn44/frontend/public/kandis -format jpeg -layers Dispose -resize 400\>x1024\> {} +
